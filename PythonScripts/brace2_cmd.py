0d16b85bd44ffd852c6bf712081f1c7e3526cba7
Amit Aviv <amit@ez3d.co>
2012-05-16 01:39:35 +0300

    ss

--- PythonScripts/brace2_cmd.py
+++ PythonScripts/brace2_cmd.py
@@ -10,10 +10,10 @@ __commandname__ = "brace2"
 
 def run(ring_rad_diff, n_ring_divisions, n_circle_divisions, in_circle_rad, out_circle_rad, div_pipe_rad, in_pipe_rad, out_pipe_rad, division_shift):
     ring_rad_diff = ring_rad_diff*3
-    n_ring_divisions = int(math.floor((14 * n_ring_divisions) + 10))
+    #n_ring_divisions = int(math.floor((14 * n_ring_divisions) + 10))
     n_circle_divisions = int(math.floor((17 * n_circle_divisions) + 3))
     in_circle_rad = (9*in_circle_rad)+1
-    out_circle_rad = (7*out_circle_rad)+1
+    out_circle_rad = (9*out_circle_rad)+1
     div_pipe_rad = (1.1 * div_pipe_rad) + 0.2
     in_pipe_rad = (1.1 * in_pipe_rad) + 0.2
     out_pipe_rad = (1.1 * out_pipe_rad) + 0.2
@@ -34,6 +34,8 @@ def run(ring_rad_diff, n_ring_divisions, n_circle_divisions, in_circle_rad, out_
    
     divs1 = rs.DivideCurve(c1, n_circle_divisions, create_points=False)
     divs2 = rs.DivideCurve(c2, n_circle_divisions, create_points=False)
+    
+    rs.DeleteObjects([c1,c2])
     q = collections.deque(divs2)
     q.rotate(division_shift)
     divs2 = list(q)
@@ -41,13 +43,27 @@ def run(ring_rad_diff, n_ring_divisions, n_circle_divisions, in_circle_rad, out_
     for p1,p2 in zip(divs1, divs2):
         l = rs.AddLine(p1,p2)
         pipe = rs.AddPipe(l, 0, div_pipe_rad, cap=2)
+        rs.DeleteObject(l)
         pipes.append(pipe)
         
     axis = rs.VectorCreate((0,0,0),(0,10,0))
     all_pipes = [pipes]
     
+    #a = rs.BooleanUnion(pipes)
+    
+    #print a
+    #return
+    bb = rs.BoundingBox(pipes)
+    circum = math.pi * 2 * ( ring_in_rad + (ring_rad_diff/2))
+    #print circum
+    width = (bb[1][0] - bb[0][0])-0.2
+    #print width
+    min_ring_divisions = max(10,math.ceil(circum/width)+1)
+    max_ring_divisions = (min_ring_divisions-1)*2
+    n_ring_divisions = int(min_ring_divisions + (n_ring_divisions * (max_ring_divisions - min_ring_divisions)))
+    
     for i in range(1,n_ring_divisions):
-        new_pipes = rs.RotateObjects(pipes, (0,0,0), i*(360/n_ring_divisions), axis=axis, copy=True)
+        new_pipes = rs.RotateObjects(pipes, (0,0,0), i*(360.0/float(n_ring_divisions)), axis=axis, copy=True)
         all_pipes.append(new_pipes)
         
     all_pipes = list(itertools.chain(*all_pipes))
@@ -59,12 +75,12 @@ def RunCommand( is_interactive ):
     go = Rhino.Input.Custom.GetOption()
     
     a1_o = Rhino.Input.Custom.OptionDouble(0.3)
-    a2_o = Rhino.Input.Custom.OptionDouble(0.5)
+    a2_o = Rhino.Input.Custom.OptionDouble(0.01)
     a3_o = Rhino.Input.Custom.OptionDouble(0.3)
-    a4_o = Rhino.Input.Custom.OptionDouble(0.6)
-    a5_o = Rhino.Input.Custom.OptionDouble(0.5)
-    a6_o = Rhino.Input.Custom.OptionDouble(0.5)
-    a7_o = Rhino.Input.Custom.OptionDouble(0.2)
+    a4_o = Rhino.Input.Custom.OptionDouble(0.1)
+    a5_o = Rhino.Input.Custom.OptionDouble(0.4)
+    a6_o = Rhino.Input.Custom.OptionDouble(0.01)
+    a7_o = Rhino.Input.Custom.OptionDouble(0.01)
     a8_o = Rhino.Input.Custom.OptionDouble(0.3)
     a9_o = Rhino.Input.Custom.OptionDouble(0.5)
      
