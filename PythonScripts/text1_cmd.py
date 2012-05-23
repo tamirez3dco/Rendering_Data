2fd94091a027e43528bcd7e5c27da9b3b6667d2e
Amit Aviv <amit@ez3d.co>
2012-05-23 22:49:53 +0300

    d

--- PythonScripts/text1_cmd.py
+++ PythonScripts/text1_cmd.py
@@ -7,14 +7,40 @@ import Rhino
 import itertools
 
 __commandname__ = "text1"
-
+def create_vase(rad, heights):
+    p1=[]
+    p2=[] 
+    
+    for r,h in zip(rad, heights):
+        p1.append((r,0,h))
+        p2.append((r-0.2,0,h))
+    
+    p2[0] = (p2[0][0], 0, p2[0][2]+0.2)
+   
+    crv1 = rs.AddInterpCurve(p1)
+    crv2 = rs.AddInterpCurve(p2)
+    mp = (p1[3][0]-0.1, 0, p1[3][2]+0.05)
+    p2.reverse()
+    axis = rs.AddLine((0,0,0),(0,0,10))
+    
+    v1 = rs.AddRevSrf(crv1, axis)
+   
+    rs.FlipSurface(v1, flip=True)
+    rs.DeleteObject(axis)
+    return (v1, crv1)
+    
 def create_text_curves(text):
     cmd = "Enter -TextObject \"%s\" 0" % (text)
-    curves = rs.Command(cmd)
-    return curves
+    rs.Command(cmd)
+    return rs.LastCreatedObjects()
 
 def RunCommand( is_interactive ):
-    create_text_curves("na")
+    create_vase([3,4,3,5],[0, 3, 6, 10])
+    
+    c = create_text_curves("na")
+    c = create_text_curves("gy")
+    c = create_text_curves("nkte")
+    c = create_text_curves("pa")
     
 if( __name__=="__main__" ):
     RunCommand(True)
