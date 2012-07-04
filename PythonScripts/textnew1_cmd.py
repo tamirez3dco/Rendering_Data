b001de7ccb4c9458543a6447dd093569fe84f5c2
Amit Aviv <amitaviv99@gmail.com>
2012-07-04 20:46:04 +0300

    jjj

--- PythonScripts/textnew1_cmd.py
+++ PythonScripts/textnew1_cmd.py
@@ -24,8 +24,8 @@ def create_text_curves(text):
 
 def create_text_surfaces(curves, letters):
     path = rs.AddLine((0,0,0),(0,0,0.1))
-    
     c_index = 0
+    res = []
     for letter in letters:
         letter_curves = curves[c_index:c_index+letter]
         letter_surfaces = []
@@ -40,17 +40,45 @@ def create_text_surfaces(curves, letters):
         c_index += letter
         
         if(len(letter_surfaces)==2):
-            rs.BooleanDifference([letter_surfaces[0]],[letter_surfaces[1]])
+            s = rs.BooleanDifference([letter_surfaces[0]],[letter_surfaces[1]])
+            res.append(s)
+        else:
+            res.append(letter_surfaces[0])
             
-        
+    return res
+
+def create_bounds(surfaces):
+    box = rs.BoundingBox(surfaces)
+    
+    path = rs.AddLine(box[0],box[4])
+    points = box[0:4]
+    points.append(box[0])
+    curves = []
+    for k in range(2):
+        p1 = rs.PointAdd(points[0], (-1*k,-1*k,0))
+        p2 = rs.PointAdd(points[1], (1*k,-1*k,0))
+        p3 = rs.PointAdd(points[2], (1*k,1*k,0))
+        p4 = rs.PointAdd(points[3], (-1*k,1*k,0))
+        np = [p1,p2,p3,p4,p1]
+        curves.append(rs.AddPolyline(np))
+        #rs.ExtrudeCurve(c0, path)
+        #rs.ExtrudeCurve(c1, path)
+    s0 = rs.ExtrudeCurve(curves[0], path)
+    s1 = rs.ExtrudeCurve(curves[1], path)
+    rs.CapPlanarHoles(s0)
+    rs.CapPlanarHoles(s1)
+    rs.BooleanDifference([s1],[s0])
+    
 def run():
     #text = "abcdefghijklmnopqrstuvwxyz"
     #text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     text = "naama"
+    path = rs.AddLine((0,0,0),(0,0,0.1))
     curve_nums = num_letter_curves(text)
     curves = create_text_curves(text)
     surfaces = create_text_surfaces(curves, curve_nums)
- 
+    create_bounds(surfaces)
+    
     return True
     
 def RunCommand( is_interactive ):
