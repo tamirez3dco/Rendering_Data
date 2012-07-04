e7ac6a14eb8d4faa144fda4d60b69cf1eba5cbed
Amit Aviv <amitaviv99@gmail.com>
2012-07-05 02:12:33 +0300

    better

--- PythonScripts/textnew1_cmd.py
+++ PythonScripts/textnew1_cmd.py
@@ -26,6 +26,7 @@ def create_text_surfaces(curves, letters):
     path = rs.AddLine((0,0,0),(0,0,0.1))
     c_index = 0
     res = []
+    
     for letter in letters:
         letter_curves = curves[c_index:c_index+letter]
         letter_surfaces = []
@@ -39,26 +40,36 @@ def create_text_surfaces(curves, letters):
             letter_surfaces.append(s)
         c_index += letter
         
-        if(len(letter_surfaces)==2):
-            s = rs.BooleanDifference([letter_surfaces[0]],[letter_surfaces[1]])
-            res.append(s)
-        else:
-            res.append(letter_surfaces[0])
+        s = letter_surfaces[0]
+        if(len(letter_surfaces)>1):
+            centroid = rs.CurveAreaCentroid(letter_curves[1])
+            print centroid
+            srf = rs.AddPlanarSrf([letter_curves[0]])
+            #rs.AddPlanarSrf(
+            ins = rs.CurveSurfaceIntersection(letter_curves[1], srf)
+            if len(ins) != 0:
+                s = rs.BooleanDifference([letter_surfaces[0]],letter_surfaces[1:len(letter_surfaces)])
+                res.append(s)
+                
+        res.append(s)
             
     return res
 
-def create_bounds(surfaces):
+def create_bounds(surfaces, width):
     box = rs.BoundingBox(surfaces)
     
     path = rs.AddLine(box[0],box[4])
     points = box[0:4]
     points.append(box[0])
     curves = []
+    margin_left = 1
+    margin_top = 0.2
+    
     for k in range(2):
-        p1 = rs.PointAdd(points[0], (-1*k,-1*k,0))
-        p2 = rs.PointAdd(points[1], (1*k,-1*k,0))
-        p3 = rs.PointAdd(points[2], (1*k,1*k,0))
-        p4 = rs.PointAdd(points[3], (-1*k,1*k,0))
+        p1 = rs.PointAdd(points[0], ((-width*k)-margin_left,(-width*k)+margin_top,0))
+        p2 = rs.PointAdd(points[1], ((width*k)+margin_left,(-width*k)+margin_top,0))
+        p3 = rs.PointAdd(points[2], ((width*k)+margin_left,(width*k)-margin_top,0))
+        p4 = rs.PointAdd(points[3], ((-width*k)-margin_left,(width*k)-margin_top,0))
         np = [p1,p2,p3,p4,p1]
         curves.append(rs.AddPolyline(np))
         #rs.ExtrudeCurve(c0, path)
@@ -69,18 +80,19 @@ def create_bounds(surfaces):
     rs.CapPlanarHoles(s1)
     rs.BooleanDifference([s1],[s0])
     
-def run(text):
-    #text = "abcdefghijklmnopqrstuvwxyz"
-    #text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    #text = "naama"
+def run(text, width):
     path = rs.AddLine((0,0,0),(0,0,0.1))
     curve_nums = num_letter_curves(text)
     curves = create_text_curves(text)
     surfaces = create_text_surfaces(curves, curve_nums)
-    create_bounds(surfaces)
+    create_bounds(surfaces, width)
     
     return True
-    
+
+def normalize_inputs(width):
+    width = width*4.5 + 0.5
+    return (width)
+
 def RunCommand( is_interactive ):
     go = Rhino.Input.Custom.GetOption()
     a1_o = Rhino.Input.Custom.OptionDouble(0.3)
@@ -93,9 +105,11 @@ def RunCommand( is_interactive ):
     a1 = a1_o.CurrentValue
     
     text = rs.GetString()
+
+    (width) = normalize_inputs(a1)
     
     rs.EnableRedraw(False)
-    run(text)
+    run(text, width)
     rs.EnableRedraw(True)
     
 if( __name__=="__main__" ):
